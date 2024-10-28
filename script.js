865963496ca6cb08d33507f363f54fcfd20c358c
Harvestum <harvestum@gmail.com>
2024-10-28 17:21:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-// Future JavaScript functions or animations can be added here
-console.log("Harvestum Exports Website Loaded");
+$(document).ready(function() {
+    let currentIndex = 0;
+    const items = $('.carousel-item');
+    const itemAmt = items.length;
+
+    function cycleItems() {
+        const item = $('.carousel-item').eq(currentIndex);
+        items.fadeOut();
+        item.fadeIn();
+    }
+
+    const autoSlide = setInterval(function() {
+        currentIndex += 1;
+        if (currentIndex > itemAmt - 1) {
+            currentIndex = 0;
+        }
+        cycleItems();
+    }, 5000);
+
+    cycleItems();
+});
